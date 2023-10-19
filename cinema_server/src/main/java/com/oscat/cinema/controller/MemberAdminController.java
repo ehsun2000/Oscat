1935e9bd80c4fdeb65ac6a0700de58c0ac0181fb
Khy108 <142572371+Khy108@users.noreply.github.com>
2023-10-20 01:43:09 +0800

    1020  修改server

--- cinema_server/src/main/java/com/oscat/cinema/controller/MemberAdminController.java
+++ cinema_server/src/main/java/com/oscat/cinema/controller/MemberAdminController.java
@@ -2,13 +2,17 @@ package com.oscat.cinema.controller;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.ArrayList;
 import java.util.Optional;
+import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.configurationprocessor.json.JSONObject;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,6 +25,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.oscat.cinema.dto.ApiResponse;
+import com.oscat.cinema.dto.MemberAdminDto;
 import com.oscat.cinema.entity.Member;
 import com.oscat.cinema.service.MemberAdminService;
 
@@ -29,25 +34,58 @@ import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/admin")
+@CrossOrigin()
 public class MemberAdminController {
-
 
 	@Autowired
 	private MemberAdminService mService;
 
-	// 找全部
-	 @GetMapping("member/all")
-	    public List<Member> getAllMembers() {
-	        List<Member> members = mService.getAllMembers();
-	        return members;
-	    }
-	
+	// 所有會員
+	@GetMapping("/member/all")
+	public ResponseEntity<List<MemberAdminDto>> getAllMembers() {
+
+		List<Member> members = mService.getAllMembers();
+
+		List<MemberAdminDto> memberDTOs = new ArrayList<>();
+		for (Member member : members) {
+			MemberAdminDto memberDTO = new MemberAdminDto();
+			memberDTO.setMemberId(member.getMemberId());
+			memberDTO.setMemberName(member.getMemberName());
+			memberDTO.setEmail(member.getEmail());
+			memberDTO.setPassword(member.getPassword());
+			memberDTO.setPhone(member.getPhone());
+			memberDTO.setGender(member.getGender());
+			memberDTO.setBirthDate(member.getBirthDate());
+			memberDTO.setJoinDate(member.getJoinDate());
+
+			memberDTOs.add(memberDTO);
+		}
+
+		return ResponseEntity.ok(memberDTOs);
+	}
+
+	// 頁面(未完成)
+	@GetMapping("/member/all/like")
+	public Optional<Member> findMemberByNameLike(@RequestParam("n") String name) {
+		return mService.findMemberByName(name);
+	}
+
+	// 檢查信箱是否被註冊過
+	@GetMapping("/member/add/{email}")
+	public ResponseEntity<?> checkEmailRegistered(@PathVariable String email) {
+		System.out.println(email);
+		Optional<Member> existingMember = mService.findMemberByEmail(email);
+		if (existingMember.isPresent()) {
+			return new ResponseEntity<String>("Y", null, HttpStatus.OK);
+		} else {
+			return new ResponseEntity<String>("N", null, HttpStatus.OK);
+		}
+	}
+
 	// 新增單筆
 	@Transactional
 	@PostMapping("/member/add")
-	@ResponseBody
-	public ResponseEntity<ApiResponse<?>> postAddCustomer3(@Valid @RequestBody Member mem,
-			BindingResult bindingResult) {
+	public ResponseEntity<?> postAddCustomer(@Valid @RequestBody Member mem, BindingResult bindingResult) {
 
 		if (bindingResult.hasErrors()) {
 			List<ObjectError> errorList = bindingResult.getAllErrors();
@@ -69,18 +107,32 @@ public class MemberAdminController {
 			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(apiResponse);
 		}
 
-		Member savedMember = mService.addMember(mem);
-		ApiResponse<Member> apiResponse = new ApiResponse<>(HttpStatus.OK.value(), "註冊成功", savedMember,
-				LocalDateTime.now().toString());
+		ResponseEntity<?> checkEmailResponse = checkEmailRegistered(mem.getEmail());
 
-		return ResponseEntity.status(HttpStatus.OK).body(apiResponse);
+		if (checkEmailResponse.getStatusCode() == HttpStatus.OK) {
+
+			Member savedMember = mService.addMember(mem);
+			ApiResponse<Member> apiResponse = new ApiResponse<>(HttpStatus.OK.value(), "註冊成功", savedMember,
+					LocalDateTime.now().toString());
+
+			return ResponseEntity.status(HttpStatus.OK).body(apiResponse);
+		} else {
+			return checkEmailResponse;
+		}
 	}
 
-	// 透過email搜尋
+	// 透過email keyword搜尋
+	@GetMapping("/member/keyEmail")
+	public String find(@RequestParam String email) {
+		List<Member> result = mService.findKeywordByEmail("%"+email+"%");
+		return result.toString();
+	}
+
+	// 透過id搜尋
 	@Transactional
-	@GetMapping("/member/byEmail/{email}")
-	public ResponseEntity<?> findByEmail(@PathVariable String email) {
-		Optional<Member> optional = mService.findMemberByEmail(email);
+	@GetMapping("/member/{memberId}")
+	public ResponseEntity<?> findById(@PathVariable UUID memberId) {
+		Optional<Member> optional = mService.findMemberById(memberId);
 
 		if (optional.isPresent()) {
 			Member result = optional.get();
@@ -90,20 +142,12 @@ public class MemberAdminController {
 		return new ResponseEntity<String>("沒有該筆資料", null, HttpStatus.BAD_REQUEST);
 	}
 
-
-	// 刪除單筆
+	// 更新
 	@Transactional
-    @DeleteMapping("/member/delete")
-    public String deleteMemberByEmail(@RequestParam String email) {
-        String result = mService.deleteMemberByEmail(email);
-        return result;
-    }
-	
-
-	@PutMapping("/member/update")
+	@PutMapping("/member/update/{memberId}")
 	@ResponseBody
-	public ResponseEntity<ApiResponse<?>> updateMemberByEmail(@Valid @RequestBody Member newMember,
-			BindingResult bindingResult) {
+	public ResponseEntity<ApiResponse<?>> updateMemberById(@Valid @RequestBody Member newMember,
+			@PathVariable UUID memberId, BindingResult bindingResult) {
 
 		if (bindingResult.hasErrors()) {
 			List<ObjectError> errorList = bindingResult.getAllErrors();
@@ -118,19 +162,21 @@ public class MemberAdminController {
 			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(apiResponse);
 		}
 
-		Optional<Member> existingMember = mService.findMemberByEmail(newMember.getEmail());
+		Optional<Member> existingMember = mService.findMemberById(newMember.getMemberId());
 
 		if (!existingMember.isPresent()) {
 			ApiResponse<?> apiResponse = new ApiResponse<>(HttpStatus.NOT_FOUND.value(), "沒有該會員",
-					"找不到該信箱: " + newMember.getEmail(), LocalDateTime.now().toString());
+					"沒有該會員編號: " + newMember.getMemberId(), LocalDateTime.now().toString());
 
 			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(apiResponse);
 		}
 
+		// 加入時間為最初新增的時間，經修改後不變
+		Member originalMember = existingMember.get();
+		newMember.setJoinDate(originalMember.getJoinDate());
+
 		try {
-
 			Optional<Member> updatedMember = mService.updateMember(newMember);
-
 			if (updatedMember.isPresent()) {
 				ApiResponse<?> apiResponse = new ApiResponse<>(HttpStatus.OK.value(), "會員資料更新成功", updatedMember,
 						LocalDateTime.now().toString());
@@ -141,13 +187,35 @@ public class MemberAdminController {
 						.body(new ApiResponse<>(HttpStatus.INTERNAL_SERVER_ERROR.value(), "會員資料更新失敗", "無法保存更新後的會員資料",
 								LocalDateTime.now().toString()));
 			}
-
 		} catch (Exception e) {
 			ApiResponse<?> apiResponse = new ApiResponse<>(HttpStatus.INTERNAL_SERVER_ERROR.value(), "會員資料更新失敗",
 					e.getMessage(), LocalDateTime.now().toString());
-
 			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(apiResponse);
 		}
+
 	}
+
+	// 刪除
+	@Transactional
+	@DeleteMapping("/member/delete/{memberId}")
+	public String deleteMemberById(@PathVariable UUID memberId) {
+
+		String result = mService.deleteMemberById(memberId);
+		return result;
+	}
+	
+	// 透過email搜尋
+//	@Transactional
+//	@GetMapping("/member/byEmail/{email}")
+//	public ResponseEntity<?> findByEmail(@PathVariable String email) {
+//		Optional<Member> optional = mService.findMemberByEmail(email);
+//
+//		if (optional.isPresent()) {
+//			Member result = optional.get();
+//			return new ResponseEntity<Member>(result, null, HttpStatus.OK);
+//		}
+//
+//		return new ResponseEntity<String>("沒有該筆資料", null, HttpStatus.BAD_REQUEST);
+//	}
 
 }
--- cinema_server/src/main/java/com/oscat/cinema/service/MemberAdminService.java
+++ cinema_server/src/main/java/com/oscat/cinema/service/MemberAdminService.java
@@ -2,6 +2,7 @@ package com.oscat.cinema.service;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -24,10 +25,26 @@ public class MemberAdminService {
         return memberRepository.save(member);
     }
 
+    // 根據name查找會員
+    public Optional<Member> findMemberByName(String name) {
+    	return memberRepository.findMemberByNameLike(name);    
+    }
+    
     // 根據email查找會員
     public Optional<Member> findMemberByEmail(String email) {
         return memberRepository.findByEmail(email);
+    }   
+    
+    // 根據id查找會員
+    public Optional<Member> findMemberById(UUID memberId) {
+		return memberRepository.findById(memberId);
     }
+    
+    // 關鍵字搜尋 byEmail
+    public List<Member> findKeywordByEmail(String email){
+    	return memberRepository.findMemberByEmailLike(email);
+    }
+    
 
     // 獲取所有會員列表
     public List<Member> getAllMembers() {
@@ -45,20 +62,51 @@ public class MemberAdminService {
         }
     }
 
-    // 更新會員資料
-    public Optional<Member> updateMember(Member newMember) {
-        Optional<Member> existingMember = memberRepository.findByEmail(newMember.getEmail());
-        if (existingMember.isPresent()) {
-            Member memberToUpdate = existingMember.get();
-            memberToUpdate.setMemberName(newMember.getMemberName());
-            memberToUpdate.setPassword(newMember.getPassword());
-            memberToUpdate.setPhone(newMember.getPhone());
-            memberToUpdate.setGender(newMember.getGender());
-            memberToUpdate.setBirthDate(newMember.getBirthDate());
-
-            return Optional.of(memberRepository.save(memberToUpdate));
+    // 根據id刪除會員
+    public String deleteMemberById(UUID memberId) {
+        Optional<Member> optionalMember = memberRepository.findById(memberId);
+        if (optionalMember.isPresent()) {
+            memberRepository.deleteById(memberId);
+            return "刪除成功";
         } else {
-            return Optional.empty();
+            return "找不到該會員";
         }
     }
+    // 更新會員資料
+    public Optional<Member> updateMember(Member newMember) {
+    	Optional<Member> existingMember = memberRepository.findById(newMember.getMemberId());
+    	if (existingMember.isPresent()) {
+    		Member memberToUpdate = existingMember.get();
+    		memberToUpdate.setMemberName(newMember.getMemberName());
+    		memberToUpdate.setEmail(newMember.getEmail());
+    		memberToUpdate.setPassword(newMember.getPassword());
+    		memberToUpdate.setPhone(newMember.getPhone());
+    		memberToUpdate.setGender(newMember.getGender());
+    		memberToUpdate.setBirthDate(newMember.getBirthDate());
+    		
+    		return Optional.of(memberRepository.save(memberToUpdate));
+    	} else {
+    		return Optional.empty();
+    	}
+    }
+    
+    // 更新會員資料
+//    public Optional<Member> updateMember(Member newMember) {
+//        Optional<Member> existingMember = memberRepository.findByEmail(newMember.getEmail());
+//        if (existingMember.isPresent()) {
+//            Member memberToUpdate = existingMember.get();
+//            memberToUpdate.setMemberName(newMember.getMemberName());
+//            memberToUpdate.setPassword(newMember.getPassword());
+//            memberToUpdate.setPhone(newMember.getPhone());
+//            memberToUpdate.setGender(newMember.getGender());
+//            memberToUpdate.setBirthDate(newMember.getBirthDate());
+//
+//            return Optional.of(memberRepository.save(memberToUpdate));
+//        } else {
+//            return Optional.empty();
+//        }
+//    }
+    
+
+
 }
