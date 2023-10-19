8c2d8668325605ac6c4f6c0a01181f44f49a2dc5
金安 <142438489+JimAnHsu@users.noreply.github.com>
2023-10-19 11:35:57 +0800

    V1.85
    現在點下確認和關閉後會更新當前狀態

--- cinema_vite/src/components/seat/SeatController.vue
+++ cinema_vite/src/components/seat/SeatController.vue
@@ -13,14 +13,15 @@
           座位名稱: {{ seat.seatName }}, 狀態: {{ seat.seatStatus }}
         </button>
       </p>
-      <div class="from" v-show="!isShow">
+      <div class="form" v-show="!isShow">
         <div v-if="isDialogVisible" id="dialog-container">
           <SeatStatusDialog
             :selectedSeatId="selectedSeat.seatId"
-            :selectedSeatStatus="selectedSeat.seatStatus"
+            :selectedSeatName="selectedSeat.seatName"
+            :selectedSeatStatus="selectedSeatStatus"
             @seatUpdated="closeSeatStatusDialog"
           />
-          <button @click="close">關</button>
+          <button @click="closeSeatStatusDialog">關</button>
         </div>
       </div>
     </div>
@@ -29,64 +30,75 @@
 </template>
 
 <script>
-import { ref } from 'vue';
+import { ref, onMounted } from 'vue';
 import SeatStatusDialog from './SeatStatus.vue';
 
 export default {
-  data() {
-    return {
-      findSeatApi: import.meta.env.VITE_API_FindSeat,
-      roomId: ref(''),
-      seats: ref([]),
-      errorMessage: '',
-      isShow: true,
-      isDialogVisible: false, // 控制彈出式視窗顯示
-      selectedSeat: null, // 存儲所選座位資訊
+  setup() {
+    const roomId = ref('');
+    const seats = ref([]);
+    const errorMessage = ref(null);
+    const isShow = ref(true);
+    const isDialogVisible = ref(false);
+    const selectedSeat = ref(null);
+    const selectedSeatStatus = ref('');
+    const showError = ref(false);
+
+    const findSeatApi = import.meta.env.VITE_API_FindSeat;
+
+    const getSeatStatus = async () => {
+      if (!roomId.value) {
+        return;
+      }
+      try {
+        const response = await fetch(`${findSeatApi}${roomId.value}`);
+        if (response.ok) {
+          const data = await response.json();
+          seats.value = data;
+          errorMessage.value = '';
+          showError.value = false;
+        } else {
+          errorMessage.value = '請求失敗：' + response.statusText;
+          showError.value = true;
+        }
+      } catch (error) {
+        errorMessage.value = '請求失敗：' + error.message;
+        showError.value = true;
+      }
     };
-  },
-  methods: {
-    async getSeatStatus() {
-      try {
-        const response = await fetch(`${this.findSeatApi}` + `${this.roomId}`);
-        if (response.ok) {
-          const data = await response.json();
-          this.seats = data;
-          this.errorMessage = '';
-        } else {
-          this.errorMessage = '請求失敗：' + response.statusText;
-        }
-      } catch (error) {
-        this.errorMessage = '請求失敗：' + error.message;
-      }
-    },
-    openSeatStatusDialog(seat) {
-      this.isShow = false;
-      this.selectedSeat = seat;
-      this.isDialogVisible = true;
-    },
-    async refreshSeatStatus(roomId) {
-      try {
-        const response = await fetch(`${this.findSeatApi}${roomId}`);
-        if (response.ok) {
-          const data = await response.json();
-          this.seats = data;
-          this.errorMessage = '';
-        } else {
-          this.errorMessage = '請求失敗：' + response.statusText;
-        }
-      } catch (error) {
-        this.errorMessage = '請求失敗：' + error.message;
-      }
-    },
-    close() {
-      this.isShow = !this.isShow;
-      this.refreshSeatStatus(this.roomId);
-    },
-    closeSeatStatusDialog() {
-      this.isDialogVisible = false;
-      this.selectedSeat = null;
-      this.close(); // 這裡調用 close 方法
-    },
+
+    const openSeatStatusDialog = (seat) => {
+      isShow.value = false;
+      selectedSeat.value = seat;
+      selectedSeatStatus.value = seat.seatStatus;
+      isDialogVisible.value = true;
+    };
+
+    const closeSeatStatusDialog = () => {
+      isDialogVisible.value = false;
+      selectedSeat.value = null;
+      selectedSeatStatus.value = '';
+      isShow.value = true;
+      getSeatStatus();
+    };
+
+    onMounted(() => {
+      getSeatStatus();
+    });
+
+    return {
+      roomId,
+      seats,
+      errorMessage,
+      isShow,
+      isDialogVisible,
+      selectedSeat,
+      selectedSeatStatus,
+      getSeatStatus,
+      openSeatStatusDialog,
+      closeSeatStatusDialog,
+      showError,
+    };
   },
   components: {
     SeatStatusDialog,
@@ -98,14 +110,12 @@ export default {
 .error-message {
   color: red;
 }
-.from {
+.form {
   width: 400px;
   height: 400px;
   background: #efefef;
   display: flex;
-  -webkit-align-items: center;
   align-items: center;
-  -webkit-justify-content: center;
   justify-content: center;
 }
 </style>
--- cinema_vite/src/components/seat/SeatStatus.vue
+++ cinema_vite/src/components/seat/SeatStatus.vue
@@ -1,46 +1,54 @@
 <template>
   <div class="modal">
+    <p>座位 ：{{ selectedSeatName }}</p>
     <h2>修改座位狀態</h2>
     <form @submit.prevent="submitForm">
       <label for="status">座位狀態：</label>
-      <input type="radio" v-model="selectedStatus" value="Normal" name="status" />
+      <input
+        type="radio"
+        v-model="selectedStatus"
+        value="Normal"
+        name="status"
+      />
       正常
-      <input type="radio" v-model="selectedStatus" value="Maintenance" name="status" />
+      <input
+        type="radio"
+        v-model="selectedStatus"
+        value="Maintenance"
+        name="status"
+      />
       維修中
-      <button type="submit" @click="closeDialog">確認</button>
+      <button type="submit">確認</button>
     </form>
   </div>
 </template>
 
 <script>
+import { ref } from 'vue';
+
 export default {
-  data() {
-    return {
-      selectedStatus: '', // 初始值為空字符串
-      seatStatusAPI: import.meta.env.VITE_API_UpdateSeatStatus,
-    };
-  },
   props: {
     selectedSeatId: String,
+    selectedSeatName: String,
     selectedSeatStatus: String,
   },
-  created() {
-    this.selectedStatus = this.selectedSeatStatus;
-  },
-  methods: {
-    submitForm() {
-      const selectedSeatId = this.selectedSeatId;
-      const status = this.selectedStatus;
+  setup(props, context) {
+    const selectedStatus = ref(props.selectedSeatStatus);
+    const seatStatusAPI = import.meta.env.VITE_API_UpdateSeatStatus;
+
+    const submitForm = () => {
+      const selectedSeatId = props.selectedSeatId;
+      const status = selectedStatus.value;
 
       if (selectedSeatId) {
-        const url = `${this.seatStatusAPI}?id=${selectedSeatId}&status=${status}`;
+        const url = `${seatStatusAPI}?id=${selectedSeatId}&status=${status}`;
 
         fetch(url, {
           method: 'PUT',
         })
           .then((response) => {
             if (response.ok) {
-              this.closeDialog();
+              context.emit('seatUpdated');
             } else {
               console.error('更新失敗：' + response.statusText);
             }
@@ -51,10 +59,12 @@ export default {
       } else {
         console.error('selectedSeatId 為 undefined');
       }
-    },
-    closeDialog() {
-      this.$emit('closeDialog'); // 傳遞關閉事件給父組件
-    },
+    };
+
+    return {
+      selectedStatus,
+      submitForm,
+    };
   },
 };
 </script>
