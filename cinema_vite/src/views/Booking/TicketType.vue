75498ef91e36dbd2a5d5d8b4cec6bb6fcabbc4d0
金安 <142438489+JimAnHsu@users.noreply.github.com>
2023-10-31 02:27:21 +0800

    V1.38

--- cinema_vite/src/views/Booking/TicketType.vue
+++ cinema_vite/src/views/Booking/TicketType.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
     <div v-for="ticket in ticketTypes" :key="ticket.typeId">
-      {{ ticket.typeName }} - 數量: {{ count }}
-      <button @click="increaseCount">票數</button>
+      {{ ticket.typeName }} - 數量: {{ ticketCounts[ticket.typeId] || 0 }}
+      <button @click="increaseCount(ticket.typeId)">票數</button>
     </div>
   </div>
 </template>
@@ -10,40 +10,37 @@
 <script>
 import { ref, onMounted } from 'vue';
 import axios from 'axios';
-// import { useRoute } from 'vue-router';
 
 export default {
   setup() {
     const ticketTypes = ref([]);
-    const count = ref(0);
+    const ticketCounts = ref({}); // 使用對象來存儲每個票種的數量
 
     const api = import.meta.env.VITE_OSCAT_API_ENDPOINT;
 
-    const fetchTicketTypes = () => {
-      axios
-        .get(`${api}/book/ticketTypes`)
-        .then((response) => {
-          ticketTypes.value = response.data;
-        })
-        .catch((error) => {
-          console.error('Error fetching ticket types:', error);
+    const fetchTicketTypes = async () => {
+      try {
+        const response = await axios.get(`${api}/book/ticketTypes`);
+        ticketTypes.value = response.data;
+
+        // 初始化每個票種的數量為 0
+        ticketTypes.value.forEach(ticket => {
+          ticketCounts.value[ticket.typeId] = 0;
         });
+      } catch (error) {
+        console.error('Error fetching ticket types:', error);
+      }
     };
 
-    const increaseCount = () => {
-      count.value += 1;
+    const increaseCount = (typeId) => {
+      ticketCounts.value[typeId]++;
     };
-    // const route = useRoute();
-    // const movieId = route.params.movieId;
-    // const cinemaId = route.params.cinemaId;
-    // const selectedDate = route.params.date;
-    // const selectedTime = route.params.time;
 
     onMounted(fetchTicketTypes);
 
     return {
       ticketTypes,
-      count,
+      ticketCounts, // 將 ticketCounts 返回到模板中
       increaseCount,
     };
   },
